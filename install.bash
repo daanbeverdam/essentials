5292e92b7d4f64634f03750e8ab5d66d280748f6
Daan Beverdam <daanbeverdam@gmail.com>
2017-07-17 16:57:04 +0200

    added global gitignore
    Also improved install script

--- install.bash
+++ install.bash
@@ -1,15 +1,26 @@
-read -p "This script will include the essential scripts in the dotfiles in your home folder. Do not run this twice! Proceed? (y/n) " -n 1 -r
+read -p "This script will include the essential scripts in the dotfiles in your home folder! Proceed? (y/n) " -n 1 -r
+echo ""
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ESSENTIALS="if [ -f ${DIR}/.bash_essentials ]; then
+    . ${DIR}/.bash_essentials
+fi"
+GITCONFIG="[include]
+    path = ${DIR}/.git_aliases"
+GITIGNORE="[core]
+    excludesfile = ${DIR}/.gitignore_global"
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "[include]
-    path = ${DIR}/.git_aliases" >> $HOME/.gitconfig
-    echo "if [ -f ${DIR}/.bash_essentials ]; then
-    . ${DIR}/.bash_essentials
-fi" >> $HOME/.bashrc
+    if ! [[ $(<$HOME/.gitconfig) = *"$GITIGNORE"* ]]; then
+        echo "${GITIGNORE}" >> $HOME/.gitconfig
+    fi
+    if ! [[ $(<$HOME/.gitconfig) = *"$GITCONFIG"* ]]; then
+        echo "${GITCONFIG}" >> $HOME/.gitconfig
+    fi
+    if ! [[ $(<$HOME/.bashrc) = *"$ESSENTIALS"* ]]; then
+        echo "${ESSENTIALS}" >> $HOME/.bashrc
+    fi
+    echo "Installed essentials. Now open a new shell and get back to work!"
 else
     echo "Script canceled!"
 fi
 
-echo ""
-echo "Installed essentials. Now open a new shell and get back to work!"
